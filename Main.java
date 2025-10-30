363d94a9e04903f5e6dc5c2fa42a7401d325e9af
NeoN43k <vova7z18@mail.ru>
2025-10-30 18:43:21 +0300

    This is edit for MacBook 4

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Easy peasy lemon squyziy");
         System.out.println("Hi with macBOOK");
         System.out.println("It`s third edit Win;)");
+        System.out.println("It`s fourth edit Mac AIR;0)");
     }
 }
